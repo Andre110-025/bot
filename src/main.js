32e026f18b5b7f195bc9f43e6ca997063932f4cb
Andre110-025 <andreharrison355@gmail.com>
2025-11-18 16:28:51 +0100

    last push

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-// my main.js currently looks like this
-// import './assets/main.css'
-
-// import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-// import App from './App.vue'
-
-// const app = createApp(App)
-
-// app.use(createPinia())
-
-// app.mount('#app')
-
 // src/main.js - The new entry file for the custom element build
 import { defineCustomElement } from 'vue'
 import AppVue from './App.vue'
@@ -36,6 +23,38 @@ const getIdElement = defineCustomElement(getUserId)
 
 // We will skip global registration of sub-components here and rely on
 // Vue/Vite's bundling for a cleaner output.
+
+function getScriptBaseUrl() {
+  // Find the current script tag by looking for the known filename
+  const scripts = document.getElementsByTagName('script')
+  for (let i = 0; i < scripts.length; i++) {
+    // Find the script tag that sourced the current file
+    if (scripts[i].src.includes('chatbot-widget.iife.js')) {
+      const url = scripts[i].src
+      // Return the path up to the final forward slash
+      return url.substring(0, url.lastIndexOf('/'))
+    }
+  }
+  return ''
+}
+
+function loadWidgetStyles(domain) {
+  if (!domain) return
+
+  const cssLink = document.createElement('link')
+  cssLink.rel = 'stylesheet'
+  cssLink.type = 'text/css'
+
+  // Construct the full URL to the CSS file
+  cssLink.href = `${domain}/chatbot-widget.css`
+  cssLink.id = 'chatbot-widget-styles'
+
+  // Prevent loading the styles multiple times
+  if (!document.getElementById(cssLink.id)) {
+    document.head.appendChild(cssLink)
+  }
+  console.log(`Loading CSS from: ${cssLink.href}`)
+}
 
 // 4. Define the Main Chatbot Custom Element
 const ChatbotElement = defineCustomElement(AppVue)
